d4ec48bb763ecb6e602331f6e4dda803ad6219d9
yeji <yeji11077@gmail.com>
2024-11-07 04:46:55 +0900

    [feat] 상하좌우대각선 로직까진 완성

--- machugi-front/src/Pages/Game.jsx
+++ machugi-front/src/Pages/Game.jsx
@@ -29,6 +29,11 @@ function Game() {
   );
 
   const handleClickNum = (lineIndex, numIndex) => {
+    // 비어있는 칸은 아예 로직 실행 안함
+    if (nums[lineIndex][numIndex] == 0) {
+      return;
+    }
+
     // 선택 표시
     const newClicked = [...clicked];
     newClicked[lineIndex][numIndex] = true;
@@ -39,7 +44,19 @@ function Game() {
       setChoicedNum([nums[lineIndex][numIndex], lineIndex, numIndex]);
     } else {
       // 이미 선택된 숫자가 있다면 해당 숫자랑 테스트해보기
-      console.log(choicedNum, nums[lineIndex][numIndex], "을 비교해요");
+      const numberCheck = checkNums(choicedNum, [
+        nums[lineIndex][numIndex],
+        lineIndex,
+        numIndex,
+      ]);
+
+      // 테스트 값이 맞으면 없애주기
+      if (numberCheck) {
+        let newNums = [...nums];
+        newNums[choicedNum[1]][choicedNum[2]] = 0;
+        newNums[lineIndex][numIndex] = 0;
+        setNums(newNums);
+      }
       setChoicedNum([]);
 
       // 선택 표시 지움
@@ -50,6 +67,49 @@ function Game() {
       );
     }
   };
+
+  // 메인 로직(num1과 num2의 정보를 바탕으로 괜찮은지 체크)
+  function checkNums(num1, num2) {
+    // 우선 둘의 숫자가 같거나, 더해서 10이 되는지 확인
+    if (num1[0] == num2[0] || num1[0] + num2[0] == 10) {
+      const directions = [
+        [1, 0],
+        [-1, 0],
+        [0, 1],
+        [0, -1],
+        [1, 1],
+        [-1, 1],
+        [1, -1],
+        [-1, -1],
+      ];
+      const [y, x] = [num1[1], num1[2]];
+
+      for (let [dy, dx] of directions) {
+        let i = 1;
+        let temp = true;
+
+        while (temp) {
+          let [ny, nx] = [y + i * dy, x + i * dx];
+          if (0 <= ny && ny < nums.length && nx >= 0 && nx < 9) {
+            if (ny === num2[1] && nx === num2[2]) {
+              return true;
+            }
+            if (nums[ny][nx] === 0) {
+              i += 1;
+            } else {
+              temp = false;
+            }
+          } else {
+            temp = false;
+          }
+        }
+      }
+
+      return false;
+    } else {
+      return false;
+    }
+  }
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-8">
